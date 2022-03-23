21c0d4e38fab3d4854010dd339d5f1e4f4505094
Gabriel <pucikplay@gmail.com>
2022-03-23 15:57:34 +0100

    Parallelized k-random

--- L1/k_random.cpp
+++ L1/k_random.cpp
@@ -42,3 +42,29 @@ std::vector<int> best_random_road(std::size_t k, std::size_t n, int** matrix) {
 
 	return best_permutation;
 }
+
+std::vector<int> best_random_road_parallel(std::size_t k, std::size_t n, int** matrix) {
+
+    std::vector<int> permutation;
+    std::vector<int> best_permutation;
+    size_t best_length = SIZE_MAX;
+    size_t length = 0;
+
+    for (size_t i = 0; i < n; i++) permutation.push_back(i);
+
+    #pragma omp parallel for num_threads(k)
+        for (size_t i = 0; i < k; i++) {
+            std::shuffle(std::begin(permutation), std::end(permutation), rng1);
+
+            length = calculate_length(permutation, matrix, n);
+
+            #pragma omp critical
+                if (length < best_length) {
+                    best_length = length;
+                    best_permutation = permutation;
+                    //printf("%ld\n", best_length);
+                }
+        }
+
+    return best_permutation;
+}
--- L1/k_random.h
+++ L1/k_random.h
@@ -10,5 +10,6 @@
 
 std::vector<int> best_random_road(size_t k, size_t n, int** matrix);
 size_t calculate_length(const std::vector<int>& permutation, int** matrix, size_t n);
+std::vector<int> best_random_road_parallel(std::size_t k, std::size_t n, int** matrix);
 
 #endif //ALME_K_RANDOM_H
