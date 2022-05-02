0903a4e53a40220228b62a61e6355284a4d7b68f
Kareshis <franek.stepek@gmail.com>
2022-05-02 23:12:19 +0200

    Multith. nei. mode added, repaired random start

--- L1/tabu_multithreaded_wrapper.cpp
+++ L1/tabu_multithreaded_wrapper.cpp
@@ -4,13 +4,13 @@
 
 #include "tabu_multithreaded_wrapper.h"
 
-void thread_tabu(std::vector<int> road, int** matrix, size_t n, size_t thread_no, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t *lengths, std::vector<int> *paths)
+void thread_tabu(std::vector<int> road, int** matrix, std::size_t n, size_t thread_no, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, int mode, size_t *lengths, std::vector<int> *paths)
 {
-    paths[thread_no] = get_tabu_road(road, matrix, n, tabuSize, time, enhancementLimit, kickRange);
+    paths[thread_no] = get_tabu_road(road, matrix, n, tabuSize, time, enhancementLimit, kickRange, mode);
     lengths[thread_no] = calculate_length(paths[thread_no], matrix, n);
 }
 
-std::vector<int> get_tabu_road_parallel(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t no_threads)
+std::vector<int> get_tabu_road_parallel(std::vector<std::pair<double, double>> coords, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, int mode, size_t no_threads)
 {
     std::vector<int> best_permutation;
     auto *lengths = new size_t[no_threads];
@@ -19,8 +19,9 @@ std::vector<int> get_tabu_road_parallel(std::vector<int> road, int** matrix, std
     std::thread th[no_threads];
 
     for (size_t i = 0; i < no_threads; i++) {
+        std::vector<int> road = best_random_road(1000, coords.size(), matrix);
         paths[i] = road;
-        th[i] = std::thread(thread_tabu, road, matrix, n, i, tabuSize, time, enhancementLimit, kickRange, lengths, paths);
+        th[i] = std::thread(thread_tabu, road, matrix, n, i, tabuSize, time, enhancementLimit, kickRange, mode, lengths, paths);
     }
 
     for (size_t i = 0; i < no_threads ; i++) {
--- L1/tabu_multithreaded_wrapper.h
+++ L1/tabu_multithreaded_wrapper.h
@@ -11,6 +11,6 @@
 #include "tabu_search.h"
 #include "k_random.h"
 
-std::vector<int> get_tabu_road_parallel(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t no_threads);
+std::vector<int> get_tabu_road_parallel(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, int mode, size_t no_threads);
 
 #endif //ALME_TABU_MULTITHREADED_WRAPPER_H
