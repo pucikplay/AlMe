34a179828f7844e9799db5104be421b381dd1990
Gabriel <pucikplay@gmail.com>
2022-05-02 21:04:30 +0200

    Small fixes

--- L1/tabu_multithreaded_wrapper.cpp
+++ L1/tabu_multithreaded_wrapper.cpp
@@ -4,15 +4,14 @@
 
 #include "tabu_multithreaded_wrapper.h"
 
-void thread_tabu(std::vector<int> road, int** matrix, size_t n, size_t thread_no, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t *lengths, std::vector<int> *paths) {
-
-    size_t length = SIZE_MAX;
-    size_t best_length = SIZE_MAX;
+void thread_tabu(std::vector<int> road, int** matrix, size_t n, size_t thread_no, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t *lengths, std::vector<int> *paths)
+{
     paths[thread_no] = get_tabu_road(road, matrix, n, tabuSize, time, enhancementLimit, kickRange);
     lengths[thread_no] = calculate_length(paths[thread_no], matrix, n);
 }
 
-std::vector<int> get_tabu_road_parallel(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t no_threads) {
+std::vector<int> get_tabu_road_parallel(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, size_t no_threads)
+{
     std::vector<int> best_permutation;
     auto *lengths = new size_t[no_threads];
     auto *paths = new std::vector<int>[no_threads];
