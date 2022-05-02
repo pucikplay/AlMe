16bb7bf4d12b7093152c30b815d3052cbeacdb26
Kareshis <franek.stepek@gmail.com>
2022-05-02 23:12:52 +0200

    header update

--- L1/tabu_search.h
+++ L1/tabu_search.h
@@ -8,11 +8,32 @@
 #include "2_opt.h"
 #include "k_random.h"
 
-std::vector<int> parametrizedKick(std::vector<int> road, size_t n, size_t shuffleSize, size_t startingPoint);
-bool checkTabuList(std::pair<size_t, size_t> *tabuList, size_t n, size_t i, size_t j);
-std::vector<int> get_tabu_road(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange);
-std::vector<int> deterministicTabu(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, int mode, int kikMode, int kikSize);
+// {roadVector, score}, {tabuList, tabuCounter}
+typedef std::pair<std::pair<std::vector<int>, size_t>, std::pair<std::pair<size_t, size_t>*, int>> structLTM;
 
-std::pair<std::vector<std::pair<int, int>>, std::vector<std::vector<int>>> get_tabu_road_visual(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange);
+// Neighborhoods
+size_t invertLength(size_t length, const std::vector<int>& road, int** matrix, size_t i, size_t j, size_t n);
+size_t insertLength(size_t length, const std::vector<int>& road, int** matrix, size_t i, size_t j, size_t n);
+size_t swapLength(size_t length, const std::vector<int>& road, int** matrix, size_t i, size_t j, size_t n);
+
+// Single move
+std::vector<int> doStep(std::vector<int> road, size_t i, size_t j, int mode);
+
+// Kicks
+std::vector<int> parametrizedKick(std::vector<int> road, size_t n, std::pair<size_t, size_t> kickRange);
+std::vector<int> deterministicKik(std::vector<int> road, int rseed, int mode, int size);
+
+// Tabu dependencies
+bool checkTabuList(std::pair<size_t, size_t> *tabuList, size_t n, size_t i, size_t j);
+std::pair<size_t, std::pair<size_t, size_t>> checkNeighbourhood(std::vector<int> road, int** matrix, std::size_t n, size_t length, std::pair<size_t, size_t>* tabuList, int elementsOnTabu, int mode, size_t globalBestLen);
+
+// Long-Term-Memory maintanance
+std::vector<structLTM> addThingToLTM(std::vector<structLTM> returnList, structLTM newThing);
+std::vector<structLTM> cutLast(std::vector<structLTM> returnList);
+
+// Tabu Algorithms
+std::vector<int> get_tabu_road(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, int mode);
+std::vector<int> deterministicTabu(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, int mode, int kikMode, int kikSize);
+std::pair<std::vector<std::pair<int, int>>, std::vector<std::vector<int>>> get_tabu_road_visual(std::vector<int> road, int** matrix, std::size_t n, int tabuSize, double time, size_t enhancementLimit, std::pair<size_t, size_t> kickRange, int mode);
 
 #endif //ALME_TABU_SEARCH_H
