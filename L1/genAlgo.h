c356a4a4a4d5e3365329b2fc06e9016bc44436ca
Kareshis <franek.stepek@gmail.com>
2022-05-24 22:48:34 +0200

    Added 2 crossover & 2 selection types

--- L1/genAlgo.h
+++ L1/genAlgo.h
@@ -7,7 +7,7 @@
 
 #include <vector>
 
-std::vector<int> geneticMain(size_t n, int** matrix, int populationSize = 20, double mutationThreshold = 0.05, int mutMode = 3, int mutationIntensification = 4, int iterations = 100, int crossMode = 0, int crossSize = 5);
-std::vector<int> geneticIslands(size_t n, int** matrix, int populationSize, double mutationThreshold, int mutMode, int muttionIntensification, int wholeIterations, int crossMode, int crossSize, int islandsNumber, int swappingInterval, int swapSize);
+std::vector<int> geneticMain(size_t n, int** matrix, int populationSize = 20, double mutationThreshold = 0.05, int mutMode = 3, int mutationIntensification = 4, int iterations = 100, int crossMode = 0, int crossSize = 5, int crossType = 1, int crossCount = 21, int selectionMode = 1);
+std::vector<int> geneticIslands(size_t n, int** matrix, int populationSize, double mutationThreshold, int mutMode, int muttionIntensification, int wholeIterations, int crossMode, int crossSize, int crossType, int crossCount, int islandsNumber, int swappingInterval, int swapSize);
 
 #endif //ALME_GENALGO_H
